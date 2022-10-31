ed854acdcac03f99b8fa9fe0ae325627cda40c20
prasanth <106507229+mechneerd@users.noreply.github.com>
2022-10-31 18:22:08 +0530

    Add files via upload

---
+++ dbtablecreation.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+$servername = "localhost";
+$username="root";
+$password="";
+$dbname = "prasanthdb";
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){ 
+	die ("connection unsuccessful".$conn->connect_error);
+		}
+
+$sql = "CREATE TABLE websitename ( web_id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,web VARCHAR(20)) ;";
+
+
+if($conn->query($sql)===TRUE){
+	echo "Table created successfully";
+}else{
+	echo "Error creating table".$conn->error;	
+}
+
+?>
---
+++ deleteweb.php
@@ -0,0 +1,29 @@
+﻿<?php 
+
+
+$id = $_GET['id'];
+
+$servername = "localhost";
+$username="root";
+$password="";
+$dbname = "prasanthdb";
+
+$con = new mysqli($servername,$username,$password,$dbname);
+
+if($con->connect_error){ 
+	die ("connection unsuccessful".$con->connect_error);
+		}
+
+
+$del = "DELETE FROM websitename WHERE web_id=$id";
+
+
+if($con->query($del)===TRUE){
+	header("Location:displaywebsitedb.php ");
+}else{
+	echo 'error deleting the selected row'.$con->error;}
+
+$con->close();
+
+
+?>
